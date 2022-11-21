321757b8974d8b5166de7a2f421276ac0736eeab
Jelco <53396500+Jelcoo@users.noreply.github.com>
2022-11-21 09:53:06 +0100

    lantern

--- main.ts
+++ main.ts
@@ -1,3 +1,16 @@
-basic.forever(function () {
-	
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 0 || receivedNumber == 1) {
+        pins.digitalWritePin(DigitalPin.P0, receivedNumber)
+    } else if (receivedNumber == 2) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        basic.pause(5000)
+        pins.digitalWritePin(DigitalPin.P0, 0)
+    }
 })
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(0)
+})
+radio.setGroup(88)
