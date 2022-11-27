d6072c4dca0f55a59d4f52971ec620ceef1c92e5
Jelco <53396500+Jelcoo@users.noreply.github.com>
2022-11-27 21:49:55 +0100

    Fix

--- main.ts
+++ main.ts
@@ -1,16 +1,12 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 0 || receivedNumber == 1) {
         pins.digitalWritePin(DigitalPin.P0, receivedNumber)
-    } else if (receivedNumber == 2) {
-        pins.digitalWritePin(DigitalPin.P0, 1)
-        basic.pause(5000)
-        pins.digitalWritePin(DigitalPin.P0, 0)
     }
 })
 input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(1)
+    pins.digitalWritePin(DigitalPin.P0, 1)
 })
 input.onButtonPressed(Button.B, function () {
-    radio.sendNumber(0)
+    pins.digitalWritePin(DigitalPin.P0, 0)
 })
 radio.setGroup(88)
