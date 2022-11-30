e47c440f89acbdcc0671f018d3675456113aed0b
Jelco <53396500+Jelcoo@users.noreply.github.com>
2022-11-30 15:58:56 +0100

    Only 1 call

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 0 || receivedNumber == 1) {
-        pins.digitalWritePin(DigitalPin.P0, receivedNumber)
+    if (receivedNumber == 1) {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+    } else if (receivedNumber == 2) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
     }
 })
 input.onButtonPressed(Button.A, function () {
